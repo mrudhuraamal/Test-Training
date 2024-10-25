c445521b83df5f29415254ecf499089113b0f1d5
amal <amal@LAPTOP-5SMNA8RP>
2024-10-25 14:55:29 +0530

    worked

---
+++ OopsEmployeeTest/src/work/OopsEmployeeTest.java
@@ -0,0 +1,17 @@
+package work;
+
+public class OopsEmployeeTest {
+	public OopsEmployeeTest(String a,String b)
+	{
+		
+	System.out.println("name "+a);
+		System.out.println("department "+b);	
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OopsEmployeeTest obj1=new OopsEmployeeTest("mrudh","ec");
+		OopsEmployeeTest obj2=new OopsEmployeeTest("maalu","cc");
+	}
+
+}
+
